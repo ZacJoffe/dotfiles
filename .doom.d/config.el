ae9562dcafd310a408d3592cd8a55d9e59e0b1d3
ZacJoffe <zacharyjoffe@gmail.com>
2019-09-04 00:37:07 -0400

    Updated private config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -10,3 +10,20 @@
   (setq cquery-executable "/usr/bin/cquery"))
 
 (setq doom-font (font-spec :family "Fira Mono" :size 18))
+
+(map! :m "/" #'swiper)
+
+;(debug-on-variable-change 'company-active-map)
+;(define-key! company-active-map
+;  "TAB" #'company-complete-selection
+;  [tab] #'company-complete-selection)
+
+(map! (:after company
+        (:map company-active-map
+          "TAB" #'company-complete-selection
+          [tab] #'company-complete-selection))
+      :m
+      "C-z" #'bury-buffer
+      "C-S-z" #'unbury-buffer)
+
+(setq projectile-project-search-path '("~/Documents/Code"))
