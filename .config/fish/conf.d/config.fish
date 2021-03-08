0215146dde3d8386df817acf34d4fa89b5cba060
Zac Joffe <zacharyjoffe@gmail.com>
2021-03-08 18:56:37 -0500

    Fish config updates

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -3,6 +3,7 @@ set -x GOROOT (go env GOROOT)
 set -x GOPATH ~/go
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 set -x PATH $PATH ~/.emacs.d/bin/
+set -x PATH $PATH ~/.local/bin/
 set -x EDITOR nvim
 
 set -gx SXHKD_SHELL /usr/bin/bash
@@ -10,6 +11,9 @@ set -gx SXHKD_SHELL /usr/bin/bash
 # fzf theme
 # modified version of https://github.com/ianchesal/nord-fzf
 set -x FZF_DEFAULT_OPTS '--color=fg:#d8dee9,bg:#2e3440,hl:#81a1c1,fg+:#d8dee9,bg+:#2e3440,hl+:#81a1c1,info:#eacb8a,prompt:#bf6069,pointer:#b48dac,marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
+
+# set manpager to bat
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # aliases
 # exa instead of ls
@@ -20,5 +24,4 @@ alias ls='exa --color=auto'
 
 # bat instead of cat
 alias cat='bat --style=plain'
-
 
